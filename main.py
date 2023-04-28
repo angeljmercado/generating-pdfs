f587946d62d40f2d1bc6db72eb192729042faf93
angeljmercado <mercadomilletangel@gmail.com>
2023-04-27 22:48:39 -0700

    Add more pages per topic

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ for index, row in csv.iterrows():
     pdf.set_text_color(100, 100, 255)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
+    for i in range(int(row["Pages"] - 1)):
+        pdf.add_page()
 
 # pdf.set_font(family="Times", size=10)
 # pdf.cell(w=10, h=12, txt="Hello There!2", align="L", ln=1)
-
 pdf.output("output.pdf")
 
